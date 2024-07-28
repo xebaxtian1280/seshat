dd5005885571cad549cb3ccb2036e7ef6a8e183a
Sebas <snap.pl1280@gmail.com>
2024-07-28 11:58:21 -0500

    Se adicionan barras de control al mapa, las cuales dibujan, modifican y eliminan puntos geograficos

--- js/capturacoordenadas.js
+++ js/capturacoordenadas.js
@@ -9,21 +9,140 @@ var layers = new GeoLayers();
 
 window.onload = init();
 
-
 function init() {
   const map = new ol.Map({
+    target: "mapLocRadication",
     view: new ol.View({
       center: [-74.1, 4.65],
       zoom: 11,
       projection: "EPSG:4326",
     }),
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM(),
-      }),
-    ],
-    target: "mapLocRadication",
+    layers: [//layers.ObtenerLayersBase()
+      
+       new ol.layer.Group({
+          title: 'Mapa base',
+          layers: layers.ObtenerLayersBase()
+          
+       }),
+       new ol.layer.Group({
+        title: 'Tematicos',
+        layers: 
+          layers.CargarGeoJson()      
+        
+     })
+
+    ]
+
   });
+
+  var vector = new ol.layer.Vector({ 
+    title:'Capa de Dibujo',
+    source: new ol.source.Vector()})
+  map.addLayer(vector)
+
+  var controlModificar = new ol.interaction.Modify({source:vector.getSource()})
+  map.addInteraction(controlModificar)
+    
+  var layerSwitcher = new ol.control.LayerSwitcher({
+    tiplabel: 'Leyenda',
+    groupSelecStyle: 'children'
+  })
+
+  map.addControl(layerSwitcher)
+
+  var mainBar = new ol.control.Bar()
+  map.addControl(mainBar)
+
+  mainBar.addControl(new ol.control.FullScreen())
+  mainBar.addControl(new ol.control.Rotate())
+  mainBar.setPosition('top-left')
+
+  var drawBar = new ol.control.Bar({
+    Group: true,
+    toggleOne:true
+  })
+
+  mainBar.addControl(drawBar)  
+
+  var controlDraw = new ol.control.Toggle({
+    title:'Ubicar Avaluo',
+    html: '<i class="fas fa-map-marker-alt"></i>',
+    active: true,
+    interaction: new ol.interaction.Draw({
+      type:'Point',
+      source: vector.getSource()
+    }),
+    onToggle: function(active){
+      console.log(active);
+    },
+    // condition: function(){
+    //   if (vector.getLength>0){
+    //     alert('Ya existe una localizacion')
+    //     return true
+    //   }else{
+    //     return false
+    //   }
+    // }
+
+  })
+
+  drawBar.addControl(controlDraw)
+
+  var controlSelect = new ol.control.Toggle({
+    title:'Seleccionar puto',
+    html: '<i class="fa-solid fa-hand-point-up"></i>',
+    interaction: new ol.interaction.Select(),
+    bar: new ol.control.Bar({
+      controls:[
+        new ol.control.TextButton({
+          title:'Eliminar',
+          html:'Eliminar',
+          handleClick: function(){
+            
+            var features = controlSelect.getInteraction().getFeatures()
+            console.log(features.getLength());
+            if(features.getLength()==0){
+              alert('Selecionar un punto')
+            }
+
+            for(var i=0, f;f=features.item(i);i++){
+
+              vector.getSource().removeFeature(f)
+
+            }
+
+            controlSelect.getInteraction().getFeatures().clear()
+
+          }
+        })
+      ]
+    })
+  })
+
+  drawBar.addControl(controlSelect)
+
+  var controlBusqueda = new ol.control.SearchFeature({
+    source: layers.vectorGeoJson,
+    property: 'id_android_store'
+  })
+
+  map.addControl(controlBusqueda)
+
+  var select = new ol.interaction.Select({})
+  map.addControl(select)
+
+  controlBusqueda.on('select', function(e){
+    select.getFeatures().clear()
+    select.getFeatures().push(e.search)
+
+    var p = e.search.getGeometry().getFirstCoordinate()
+    map.getView().animate({
+      center:p,
+      zoom:19
+    })
+
+  })
+
   const divUbicacionInmueble = document.getElementById("titleCoorSelected");
   const contUbicacioninm = document.querySelector(".seleccionLocalizacion");
 
@@ -32,14 +151,15 @@ function init() {
     positioning: "left",
   });
 
-  const dibujarPunto  = new ol.interaction.Draw({
-    type:'Point'
-  })
+  // const dibujarPunto  = new ol.interaction.Draw({
+  //   type:'Point',
+  //   source: vector.getSource()
+  // })
 
-  map.addInteraction(dibujarPunto)
+  // map.addInteraction(dibujarPunto)
 
   btnGuardar.onclick = function(){
-    console.log(coordenadasInmueble.coordinate[0].toString());
+
     longitudInmuebleSolicitud.value = coordenadasInmueble.coordinate[0].toString()
     latitudInmuebleSolicitud.value = coordenadasInmueble.coordinate[1].toString()
 
@@ -57,11 +177,13 @@ function init() {
 
   map.addOverlay(inmuebleCoordenada);
 
-  map.on("click", function (e) {
-    coordenadasInmueble = e;
-    inmuebleCoordenada.setPosition(coordenadasInmueble.coordinate);
+  // map.on("click", function (e) {
 
-    divUbicacionInmueble.innerHTML = coordenadasInmueble.coordinate;
+  
+  //     coordenadasInmueble = e;
+  //   inmuebleCoordenada.setPosition(coordenadasInmueble.coordinate);
+  //   divUbicacionInmueble.innerHTML = coordenadasInmueble.coordinate;
     
-  });
+    
+  // });
 }
--- js/lib/ol-v9.2.4/GeoTools/geoLayers.js
+++ js/lib/ol-v9.2.4/GeoTools/geoLayers.js
@@ -1,35 +1,85 @@
 function GeoLayers(){
-
+  this.vectorGeoJson = null
 }
 
 GeoLayers.prototype.ObtenerLayersBase = function(){
     var listaLayers =[];
 
-    var mapasBase = new ol.layer.Group({
-        title:'Mapa Base',
-        layers:[
-          new ol.layer.Tile({
-            title: "Hybrid",
-            type: 'base',
-            source: new ol.source.XYZ({
-              url: "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-            }),
-          }),
-          new ol.layer.Tile({
-            // A layer must have a title to appear in the layerswitcher
-            title: 'OSM',
-            // Again set this layer as a base layer
-            type: 'base',
-            visible: true,
-            source: new ol.source.OSM()
-          })
-        ]
+      var hybrid = new ol.layer.Tile({
+        title: "Hybrid",
+        baseLayer:true,
+        visible:false,
+        source: new ol.source.XYZ({
+          url: "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+        }),
       })
 
-      listaLayers.push(mapasBase)
+      listaLayers.push(hybrid)
+
+      var oms = new ol.layer.Tile({
+        // A layer must have a title to appear in the layerswitcher
+        title: 'OSM',
+        // Again set this layer as a base layer
+        baseLayer:true,
+        visible: true,
+        source: new ol.source.OSM()
+      })
+
+
+      listaLayers.push(oms)
 
       return listaLayers
+}
+
+GeoLayers.prototype.CargarLayersTematicos = function(){
+  var listaTematicos = []
+
+  var lyrAvaluo = new ol.layer.Tile({
+    title:'Avaluo',
+    visible: true,
+    source: new ol.source.TileWMS({
+      url: 'http://localhost:8080/geoserver/Avaluos/wms',
+          params: {'FORMAT': 'image/png',
+                   'VERSION': '1.1.1',  
+                "STYLES": '',
+                "LAYERS": 'Avaluos:Ofertas',
+          }
+    })
+  })
+
+  listaTematicos.push(lyrAvaluo)
+
+  return listaTematicos
+  /*return new ol.layer.Group({
+    title: 'Tematicos',
+    layers: listaTematicos
+  })*/
 
 }
+
+GeoLayers.prototype.CargarGeoJson = function(){
+  var lista = []
+
+  this.vectorGeoJson = new ol.source.Vector({
+    url: '/Recursos/punto.json',
+    format: new ol.format.GeoJSON()
+  })
+
+  var lyrGeoJson = new ol.layer.Vector({
+    title: 'Avaluos',
+    style: new ol.style.Style({
+      image: new ol.style.Icon({
+        src:'/Recursos/Iconos/Casas/house.png'
+      })
+    }),
+    source: this.vectorGeoJson
+  })
+
+  lista.push(lyrGeoJson)
+
+  return lista
+}
+
+
     
 
